dec2da72af4414609a76a310f0cb07bb739678d3
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-22 05:39:54 +0400

    created HomeVm ViewModel to hold sliders and products data

---
+++ ProniaFrontToBack/ViewModels/HomeVm.cs
@@ -0,0 +1,9 @@
+﻿using ProniaFrontToBack.Models;
+
+namespace ProniaFrontToBack.ViewModels;
+
+public class HomeVm
+{
+    public List<Slider> Sliders { get; set; }
+    public List<Product> Products { get; set; }
+}
