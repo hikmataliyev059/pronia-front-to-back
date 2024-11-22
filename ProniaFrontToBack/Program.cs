a18f97873541549afaee47932f8128e559548009
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-22 05:45:43 +0400

    configured Program.cs with DbContext, MVC, and static file support

---
+++ ProniaFrontToBack/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProniaFrontToBack.DAL;
+
+namespace ProniaFrontToBack;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<AppDbContext>(opt => 
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DB_URL"))
+            );
+        
+        var app = builder.Build();
+
+        app.MapControllerRoute(
+            name:"default",
+            pattern:"{controller=Home}/{action=Index}/{id?}"
+            );
+        
+        app.UseStaticFiles();
+        
+        app.Run();
+    }
+}
