5e15b4ae1cc2646ef10ac429135132b1e8a404f5
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-22 05:45:18 +0400

    implemented Product model with properties and relationships to Category, TagProduct, and ProductImage

---
+++ ProniaFrontToBack/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using ProniaFrontToBack.Models.Base;
+
+namespace ProniaFrontToBack.Models;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string SKU { get; set; }
+    public double Price { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public List<TagProduct> TagProducts { get; set; }
+    public List<ProductImage> ProductImages { get; set; }
+}
