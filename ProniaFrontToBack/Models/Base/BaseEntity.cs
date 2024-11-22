2442e15c332fbf6b68bb1a4d6db738b3ace35d52
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-22 05:35:29 +0400

    made BaseEntity abstract and inherited it across all models, retaining Id property

---
+++ ProniaFrontToBack/Models/Base/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ProniaFrontToBack.Models.Base;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    
+   // public DateTime CreateDate { get; set; }  yaradilma vaxtini, yeni BaseEntity ne vaxt yaradilibi onu saxliyiriq
+   // public DateTime UpdateDate { get; set; }  deyisilme vaxtini 
+   // heleki bu 2 si lazim deyil deye bele yaziriq  
+}
