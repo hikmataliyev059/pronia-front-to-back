4b65f812be9077973f324550e2fa0386148b9fe8
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-22 05:42:55 +0400

    implemented Slider model with validation for title and additional properties

---
+++ ProniaFrontToBack/Models/Slider.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ProniaFrontToBack.Models.Base;
+
+namespace ProniaFrontToBack.Models;
+
+public class Slider : BaseEntity
+{
+    [Required, StringLength(20, ErrorMessage = "Length must be 20 characters or less.")]
+    public string Title { get; set; }
+
+    public string SubTitle { get; set; }
+    public string Description { get; set; }
+}
