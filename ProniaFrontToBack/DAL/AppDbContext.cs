1b145f43869105ce5b113c07e5b4d0c7308a320c
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-22 05:33:24 +0400

    implemented DbSets for core entities in AppDbContext

---
+++ ProniaFrontToBack/DAL/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProniaFrontToBack.Models;
+
+namespace ProniaFrontToBack.DAL;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Slider> Sliders { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    public DbSet<TagProduct> TagProducts { get; set; }
+    
+    public DbSet<ProductImage> ProductImages { get; set; }
+}
