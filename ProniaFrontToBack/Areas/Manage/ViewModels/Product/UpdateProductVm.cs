055e6880955e86b85f3bcb1d6c7e06c930d18cf9
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-02 16:47:26 +0400

    added UpdateProductVm for product update functionality.

---
+++ ProniaFrontToBack/Areas/Manage/ViewModels/Product/UpdateProductVm.cs
@@ -0,0 +1,11 @@
+﻿namespace ProniaFrontToBack.Areas.Manage.ViewModels.Product;
+
+public record UpdateProductVm
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public int? CategoryId { get; set; }
+    public List<int>? TagIds { get; set; }
+}
