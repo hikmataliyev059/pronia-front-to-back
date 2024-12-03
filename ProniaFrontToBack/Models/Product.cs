d6e83e36ac5b2fb0d32f20d5f5c8801d8766e8ce
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-03 04:42:05 +0400

    create product model with essential properties

--- ProniaFrontToBack/Models/Product.cs
+++ ProniaFrontToBack/Models/Product.cs
@@ -8,8 +8,8 @@ public class Product : BaseEntity
     public string Description { get; set; }
     public string SKU { get; set; }
     public double Price { get; set; }
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
-    public List<TagProduct> TagProducts { get; set; }
-    public List<ProductImage> ProductImages { get; set; }
+    public int? CategoryId { get; set; }
+    public Category? Category { get; set; }
+    public List<TagProduct>? TagProducts { get; set; }
+    public List<ProductImage>? ProductImages { get; set; }
 }
