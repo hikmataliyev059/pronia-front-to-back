8573aaa1f92e73c4703c7f41abd6eca745377867
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-10 23:41:08 +0400

    add email template helper with placeholder replacement functionality

---
+++ ProniaFrontToBack/Helpers/Email/EmailTemplateHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace ProniaFrontToBack.Helpers.Email;
+
+public static class EmailTemplateHelper
+{
+    public static string GetTemplate(string filePath, Dictionary<string, string> placeholders)
+    {
+        var template = File.ReadAllText(filePath);
+        foreach (var placeholder in placeholders)
+        {
+            template = template.Replace($"{{{{{placeholder.Key}}}}}", placeholder.Value);
+        }
+        return template;
+    }
+}
