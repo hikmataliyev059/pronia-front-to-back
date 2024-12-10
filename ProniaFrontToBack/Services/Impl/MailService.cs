fb219c11e4a122e4ccb16f0b0c3ad17efd6412be
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-10 23:44:25 +0400

    implement mail service to send emails with attachments using MailKit

---
+++ ProniaFrontToBack/Services/Impl/MailService.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using ProniaFrontToBack.Helpers.Email;
+using ProniaFrontToBack.Services.Abstractions;
+
+namespace ProniaFrontToBack.Services.Impl;
+
+public class MailService : IMailService
+{
+    private readonly MailSettings _mailSettings;
+
+    public MailService(IOptions<MailSettings> mailSettings)
+    {
+        _mailSettings = mailSettings.Value;
+    }
+
+    public async Task SendEmailAsync(MailRequest mailRequest)
+    {
+        var email = new MimeMessage();
+        email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+        email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+        email.Subject = mailRequest.Subject;
+        var builder = new BodyBuilder();
+        if (mailRequest.Attachments != null)
+        {
+            byte[] fileBytes;
+            foreach (var file in mailRequest.Attachments)
+            {
+                if (file.Length > 0)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        await file.CopyToAsync(ms);
+                        fileBytes = ms.ToArray();
+                    }
+
+                    builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
+                }
+            }
+        }
+
+        builder.HtmlBody = mailRequest.Body;
+        email.Body = builder.ToMessageBody();
+        using var smtp = new SmtpClient();
+        await smtp.ConnectAsync(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+        await smtp.AuthenticateAsync(_mailSettings.Mail, _mailSettings.Password);
+        await smtp.SendAsync(email);
+        await smtp.DisconnectAsync(true);
+    }
+}
