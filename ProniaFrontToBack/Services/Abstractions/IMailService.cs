22039effe8561c8c199bb91780bbf420e72f2841
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-10 23:42:53 +0400

    create IMailService interface for email sending functionality

---
+++ ProniaFrontToBack/Services/Abstractions/IMailService.cs
@@ -0,0 +1,8 @@
+﻿using ProniaFrontToBack.Helpers.Email;
+
+namespace ProniaFrontToBack.Services.Abstractions;
+
+public interface IMailService
+{
+    Task SendEmailAsync(MailRequest mailRequest);
+}
