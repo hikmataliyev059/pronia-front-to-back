6e83e9eaeae4f048c70c0899be79b7e5c7b977c3
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-10 23:43:40 +0400

    add login viewmodel for account login functionality

---
+++ ProniaFrontToBack/ViewModels/Account/LoginVm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProniaFrontToBack.ViewModels.Account;
+
+public class LoginVm
+{
+    [Required] public string EmailOrUsername { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
