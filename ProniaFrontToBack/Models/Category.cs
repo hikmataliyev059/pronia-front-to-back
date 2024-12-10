5fde45d4429aa2406ee61913a74c6824cab04f44
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-10 23:29:58 +0400

    update category model with base entity and data annotations

--- ProniaFrontToBack/Models/Category.cs
+++ ProniaFrontToBack/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿using ProniaFrontToBack.Models.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using ProniaFrontToBack.Models.Base;
 
 namespace ProniaFrontToBack.Models;
 
 public class Category : BaseEntity
 {
+    [Required, StringLength(10, ErrorMessage = "You can enter a maximum of 10 characters"),
+     MinLength(3, ErrorMessage = "The name must be at least 3 characters long")]
     public string Name { get; set; }
-    public List<Product> Products { get; set; }
+
+    public List<Product>? Products { get; set; }
 }
