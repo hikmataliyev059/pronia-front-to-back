1047a4f1e86d15dfad438077fb03ac82eb45266d
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-10 23:50:49 +0400

    integrate identity and email services in program configuration

--- ProniaFrontToBack/Program.cs
+++ ProniaFrontToBack/Program.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ProniaFrontToBack.DAL;
+using ProniaFrontToBack.Helpers.Email;
+using ProniaFrontToBack.Models;
+using IMailService = ProniaFrontToBack.Services.Abstractions.IMailService;
+using MailService = ProniaFrontToBack.Services.Impl.MailService;
 
 namespace ProniaFrontToBack;
 
@@ -9,25 +14,44 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews();
-        
-        builder.Services.AddDbContext<AppDbContext>(opt => 
+
+        builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+        builder.Services.AddTransient<IMailService, MailService>();
+
+        builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
+        {
+            opt.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._";
+            opt.User.RequireUniqueEmail = true;
+            // opt.SignIn.RequireConfirmedEmail = true;
+            opt.Password.RequiredLength = 8;
+            opt.Lockout.AllowedForNewUsers = true;
+            opt.Lockout.MaxFailedAccessAttempts = 3;
+            opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+        }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+        builder.Services.AddDbContext<AppDbContext>(opt =>
             opt.UseSqlServer(builder.Configuration.GetConnectionString("DB_URL"))
-            );
-        
+        );
+
         var app = builder.Build();
 
-        app.MapControllerRoute(
-            name:"areas",
-            pattern : "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
-            );
+        //deyesen bunlari yazmasanda arxada goturur bunu 34-35 deyirem.
+        app.UseAuthentication();
+        app.UseAuthorization();
 
         app.MapControllerRoute(
-            name:"default",
-            pattern:"{controller=Home}/{action=Index}/{id?}"
-            );
-        
+            name: "areas",
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+        );
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+
         app.UseStaticFiles();
-        
+
         app.Run();
     }
 }
