3065977ac132d131b2e1825968a35c5fb9fb60d3
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-14 22:56:21 +0400

    add CartVm record to represent the structure of items in the cart

---
+++ ProniaFrontToBack/ViewModels/Basket/CartVm.cs
@@ -0,0 +1,10 @@
+﻿namespace ProniaFrontToBack.ViewModels.Basket;
+
+public record CartVm
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string? ImgUrl { get; set; }
+    public int Count { get; set; }
+}
