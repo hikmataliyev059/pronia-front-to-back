6a9f7742115f7109e3d3d6c3e26a0d5c3fdf2f0b
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-17 01:29:07 +0400

    updated ShopVm with additional properties

--- ProniaFrontToBack/ViewModels/Shop/ShopVm.cs
+++ ProniaFrontToBack/ViewModels/Shop/ShopVm.cs
@@ -4,6 +4,9 @@ namespace ProniaFrontToBack.ViewModels.Shop;
 
 public class ShopVm
 {
+    public int? Order { get; set; }
+    public int? CategoryId { get; set; }
+    public string? Search { get; set; }
     public List<Product> Products { get; set; }
     public List<Category> Categories { get; set; }
 }
