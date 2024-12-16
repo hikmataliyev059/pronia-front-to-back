67e5c7fe33311e412217678e131d3381565876da
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-16 23:42:48 +0400

    added mapping configuration for creating a product entity from CreateProductVm using automapper

---
+++ ProniaFrontToBack/MapProfile/MapProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ProniaFrontToBack.Areas.Manage.ViewModels.Product;
+using ProniaFrontToBack.Models;
+
+namespace ProniaFrontToBack.MapProfile;
+
+public class MapProfiles : Profile
+{
+    public MapProfiles()
+    {
+        CreateMap<CreateProductVm, Product>();
+    }
+}
