8ebb5f06be85babeacd89bdd2e7fbc0db52a6dd8
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-16 23:45:01 +0400

    added additional properties to the AppUser model for user verification and tracking purposes

--- ProniaFrontToBack/Models/AppUser.cs
+++ ProniaFrontToBack/Models/AppUser.cs
@@ -6,4 +6,9 @@ public class AppUser : IdentityUser
 {
     public string Name { get; set; }
     public string SurName { get; set; }
+    public string? VerificationCode { get; set; }
+    public DateTime? VerificationCodeExpireTime { get; set; }
+    public DateTime? VerificationCodeGeneratedAt { get; set; }
+    public int? AccessFailedCount { get; set; }
+    public bool IsVerified { get; set; }
 }
