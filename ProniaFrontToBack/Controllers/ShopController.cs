447c5ff39085b0d11f87572b96db4be0f136b554
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-16 21:53:28 +0400

    add search functionality to ShopController index action

---
+++ ProniaFrontToBack/Controllers/ShopController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProniaFrontToBack.DAL;
+using ProniaFrontToBack.Models;
+using ProniaFrontToBack.ViewModels.Shop;
+
+namespace ProniaFrontToBack.Controllers;
+
+public class ShopController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ShopController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index(string? search)
+    {
+        IQueryable<Product> query = _context.Products
+            .Include(p => p.ProductImages)
+            .AsQueryable();
+
+        if (!String.IsNullOrEmpty(search))
+        {
+            query = query.Where(p => p.Name.ToLower().Contains(search.ToLower()));
+        }
+
+        ShopVm shopVm = new ShopVm()
+        {
+            Categories = await _context.Categories.Include(c => c.Products).ToListAsync(),
+            Products = await query.ToListAsync(),
+        };
+        return View(shopVm);
+    }
+}
