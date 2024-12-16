e8ee319ea5ec0a35493ca665a2deb5d01b7a89bb
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-16 21:54:23 +0400

    create ShopVm with products and categories properties

---
+++ ProniaFrontToBack/ViewModels/Shop/ShopVm.cs
@@ -0,0 +1,9 @@
+﻿using ProniaFrontToBack.Models;
+
+namespace ProniaFrontToBack.ViewModels.Shop;
+
+public class ShopVm
+{
+    public List<Product> Products { get; set; }
+    public List<Category> Categories { get; set; }
+}
