5c3d13ef8851a37b1661fe21661dbdf6cb3882ed
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-17 04:37:26 +0400

    add LayoutService to fetch settings from the database

---
+++ ProniaFrontToBack/Services/LayoutService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProniaFrontToBack.DAL;
+
+namespace ProniaFrontToBack.Services;
+
+public class LayoutService
+{
+    private readonly AppDbContext _context;
+
+    public LayoutService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Dictionary<string, string>> GetSettings()
+    {
+        Dictionary<string, string> setting = await _context.Settings
+            .ToDictionaryAsync(s => s.Key, s => s.Value);
+        return setting;
+    }
+}
