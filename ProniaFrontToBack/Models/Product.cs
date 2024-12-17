b6508b5084614cdf30e17e833e3c902860f6c172
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-17 23:52:19 +0400

    add new property to Product model for extended functionality

--- ProniaFrontToBack/Models/Product.cs
+++ ProniaFrontToBack/Models/Product.cs
@@ -8,6 +8,7 @@ public class Product : BaseEntity
     public string Description { get; set; }
     public string SKU { get; set; }
     public double Price { get; set; }
+    public int Count { get; set; }
     public int? CategoryId { get; set; }
     public Category? Category { get; set; }
     public List<TagProduct>? TagProducts { get; set; }
