c5d6aebb4e8c5256ce117ce7d889cdb71ee38b9e
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-17 04:38:50 +0400

    register LayoutService in dependency injection container

--- ProniaFrontToBack/Program.cs
+++ ProniaFrontToBack/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using ProniaFrontToBack.DAL;
 using ProniaFrontToBack.Helpers.Email;
 using ProniaFrontToBack.Models;
+using ProniaFrontToBack.Services;
+using ProniaFrontToBack.Services.Managers;
 using IMailService = ProniaFrontToBack.Services.Abstractions.IMailService;
 using MailService = ProniaFrontToBack.Services.Impl.MailService;
 
@@ -14,31 +16,38 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews();
+        builder.Services.AddSession();
 
         builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
         builder.Services.AddTransient<IMailService, MailService>();
+
+        builder.Services.AddScoped<SignInManager<AppUser>, CustomSignInManager>();
 
         builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
         {
             opt.User.AllowedUserNameCharacters =
                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._";
             opt.User.RequireUniqueEmail = true;
-            // opt.SignIn.RequireConfirmedEmail = true;
+            opt.SignIn.RequireConfirmedEmail = true;
             opt.Password.RequiredLength = 8;
             opt.Lockout.AllowedForNewUsers = true;
             opt.Lockout.MaxFailedAccessAttempts = 3;
             opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
         }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
+        builder.Services.AddAutoMapper(typeof(Program));
+
         builder.Services.AddDbContext<AppDbContext>(opt =>
             opt.UseSqlServer(builder.Configuration.GetConnectionString("DB_URL"))
         );
 
+        builder.Services.AddScoped<LayoutService>();
+
         var app = builder.Build();
 
-        //deyesen bunlari yazmasanda arxada goturur bunu 34-35 deyirem.
         app.UseAuthentication();
         app.UseAuthorization();
+        app.UseSession();
 
         app.MapControllerRoute(
             name: "areas",
