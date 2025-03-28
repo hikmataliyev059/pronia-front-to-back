12c63bc1749e02175a1c461ef023ecf958509fdf
hikmataliyev059 <hikmtaliyev444@gmail.com>
2025-03-28 19:07:09 +0400

    updated aspNetUsers table: made accessFailedCount nullable, added verification code generated

---
+++ ProniaFrontToBack/Migrations/20241214101709_UpdateTableColumn.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProniaFrontToBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AccessFailedCount",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerificationCodeGeneratedAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerificationCodeGeneratedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccessFailedCount",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
